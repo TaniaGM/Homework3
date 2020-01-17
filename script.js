32b6bafb4299f1c6348a8bac9416d234b9a716a0
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-01-17 18:19:55 -0500

    updating js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // Grabbing upper and lower case letters from character set
 
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(math.Random()) *26 + 65)
+}
+
 function getRandomLower(){
-    return String.fromCharCode (97);
+    return String.fromCharCode(Math.floor(math.Random()) *26 + 97);
+}
 
-    console.log((Math.floor(Math.random() *26);
+function getRandomNumber(){
+    return +String.fromCharCode(Math.floor(math.Random)) *10 +  48);
+}
 
+function getRandomSymbol(){
+    const symbols = '!@#$%^&*()_+{}|:<>?-=[]\;,./'
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
