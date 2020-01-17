63ff966b30304ac63eecd0031f704a04ae1b4ac5
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-01-17 18:39:02 -0500

    adding elements to JS

--- script.js
+++ script.js
@@ -9,10 +9,19 @@ function getRandomLower(){
 }
 
 function getRandomNumber(){
-    return +String.fromCharCode(Math.floor(math.Random)) *10 +  48);
+    return +String.fromCharCode(Math.floor(math.Random()) *10 +  48);
 }
 
 function getRandomSymbol(){
     const symbols = '!@#$%^&*()_+{}|:<>?-=[]\;,./'
     return symbols[Math.floor(Math.random() * symbols.length)];
-}
+}ß
+
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercasetEl = document.getElementById('uppercase');
+const lowercasetEl = document.getElementById('lowercase');
+const numberstEl = document.getElementById('numbers');
+const resultEl = document.getElementById('result');
+const resultEl = document.getElementById('result');
+const resultEl = document.getElementById('result');
