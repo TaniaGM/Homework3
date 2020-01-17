56f654a0d08b48101699131734698f2693cf52bf
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-01-17 16:48:00 -0500

    Updating index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+// Grabbing upper and lower case letters from character set
+
+function getRandomLower(){
+    return String.fromCharCode (97);
+
+    console.log((Math.floor(Math.random() *26);
+
+}
