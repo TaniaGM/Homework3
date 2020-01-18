3853c5057e080885eced068c6b87fdbfcb278d8c
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-01-18 08:14:32 -0500

    latest trainwreck

--- script.js
+++ script.js
@@ -8,27 +8,15 @@ const numberstEl = document.getElementById('numbers');
 const symboltEl = document.getElementById('symbols');
 const generatetEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
-const randomCollection = { lower: getRandomLower, upper: getRandomUpper, number: getRandomNumber, symbol: getRandomSymbol }
+const funcName = { 
+    lower: getRandomLower, 
+    upper: getRandomUpper, 
+    number: getRandomNumber, 
+    symbol: getRandomSymbol }
 
 //adding Event Listeners
 
-/*
-clipboard.addEventListener('click', () => {
-    const genpwd = document.createElement('genpwd');
-    const password = resultEl.innerText;
-
-    if(!password) { return; }
-
-    genpwd.value=password;
-    document.body.appendChild(genpwd);
-    genpwd.select();
-    document.execCommand('copy');
-    genpwd.remove();
-    alert('Password copied to clipboard');
-});
-
-
-generate.addEventListener('click',() => {
+generate.addEventListener('click', () => {
     const length = +lengthEl.value;
     const hasUpper = uppercaseEl.checked;
     const hasLower = lowercaseEl.checked;
@@ -36,6 +24,20 @@ generate.addEventListener('click',() => {
     const hasSymbol = symbolsEl.checked;
 
     resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+clipboard.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if(!password) { return; }
+
+    textarea.value=password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard');
 });
 
 //adding function codes
@@ -53,40 +55,32 @@ function generatePassword(upper, lower, number, symbol, length) {
 //looping
 for(let i=0; i<length; i+=typesCount) {
     typesArr.forEach(type => {
-        const randomCollection = Object.keys(type)[0];
-        generatedPassword += randomCollection[randomCollection]
-        ();
+        const funcName = Object.keys(type)[0];
+        generatedPassword += funcName[funcName]();
     });
+
 }
-const finalPassword = generatedPassword.slice(0,length);
+/*const finalPassword = generatedPassword.slice(0,length);
 return finalPassword;
 }
+*/
 
-// Grabbing upper and lower case letters from character set
+// Grabbing upper and lower case letters from character set (http://www.net-comber.com/charset.html)
 
 function getRandomUpper() {}
-
-    return String.fromCharCode(Math.floor(math.Random()) *26 + 65)
+    return String.fromCharCode(Math.floor(Math.random()) *26 + 65);
 }
- console.log(String.fromCharCode(97));
+ 
 
 function getRandomLower(){}
-
-console.log(String.fromCharCode(99));
-
-/*
-    return String.fromCharCode(Math.floor(math.Random()) *26 + 97);
+    return String.fromCharCode(Math.floor(Math.random()) *26 + 97);
 }
 
 function getRandomNumber(){
-    return +String.fromCharCode(Math.floor(math.Random()) *10 +  48);
+    return +String.fromCharCode(Math.floor(Math.random()) *10 +  48);
 }
 
 function getRandomSymbol(){
     const symbols = '!@#$%^&*()_+{}|:<>?-=[]\;,./'
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-
-
-*/
